57e5cf94ce9d736f5716b71b8b7ca9cf95dbfb95
Seo Heewon <seoheewon123@hufs.ac.kr>
2023-05-30 14:24:06 +0900

    Revert "change-post-lists"
    This reverts commit 844a823a7fcebeacaa2bab9caab2f83f3e7176bf.

--- wasm/src/components/community/CommunityList.tsx
+++ wasm/src/components/community/CommunityList.tsx
@@ -1,20 +1,9 @@
 import { NavLink } from 'react-router-dom'
 import classes from './CommunityList.module.css'
-import { useState } from 'react'
-import styled from 'styled-components'
 function CommunityList({ posts, commentAmount }) {
-  const [page, setPage] = useState(1)
   const reversePosts = posts.slice().reverse()
   const reverseCommentAmount = commentAmount.slice().reverse()
-  const postLists = Math.ceil(posts.length / 10)
-  const nowPosts = []
-  for (let j = (page - 1) * 10; j < (page - 1) * 10 + 10; j++) {
-    if (j < posts.length) nowPosts.push(reversePosts[j])
-  }
-  const newPosts = JSON.parse(JSON.stringify(nowPosts))
 
-  const showedPosts = new Array(postLists).fill(0)
-  console.log(newPosts)
   return (
     <div>
       <h1 style={{ marginRight: '60%' }}>목록</h1>
@@ -29,7 +18,7 @@ function CommunityList({ posts, commentAmount }) {
           </tr>
         </thead>
         <tbody>
-          {newPosts.map((post, index) => (
+          {reversePosts.map((post, index) => (
             <tr key={post.id}>
               <td>{post.id}</td>
               <td>{post.writer}</td>
@@ -43,43 +32,8 @@ function CommunityList({ posts, commentAmount }) {
           ))}
         </tbody>
       </table>
-      {showedPosts.map((item, index) => (
-        <Button key={index + 1} onClick={() => setPage(index + 1)}>
-          {index + 1}
-        </Button>
-      ))}
     </div>
   )
 }
-
-const Button = styled.button`
-  overflow: hidden;
-  position: relative;
-  height: 38px;
-  display: inline-block;
-  zoom: 1;
-  _display: block;
-  text-align: center;
-  -webkit-box-sizing: border-box;
-
-  position &:hover {
-    background: gray;
-    cursor: pointer;
-    transform: translateY(-2px);
-  }
-
-  &[disabled] {
-    background: grey;
-    cursor: revert;
-    transform: revert;
-  }
-
-  &[aria-current] {
-    background: deeppink;
-    font-weight: bold;
-    cursor: revert;
-    transform: revert;
-  }
-`
 
 export default CommunityList
